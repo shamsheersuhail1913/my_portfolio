c3e188f21916c94c00e20decf5a44a1440a589b3
shamshersuhail <shamshersuhail786@gmail.com>
2025-11-26 22:35:28 +0530

    Add complete portfolio website with 3D animations and responsive design

---
+++ upload-to-github.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Creating GitHub repository...
+echo.
+echo 1. Go to https://github.com/new
+echo 2. Repository name: portfolio-website
+echo 3. Make it public
+echo 4. Don't initialize with README
+echo 5. Click "Create repository"
+echo.
+echo Then replace YOUR_USERNAME below with your GitHub username and run:
+echo.
+echo git remote set-url origin https://github.com/YOUR_USERNAME/portfolio-website.git
+echo git push -u origin main
+echo.
+pause
